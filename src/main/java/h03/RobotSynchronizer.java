17e3b2cc71ccb2b9f6fda7d2d6f7d96533dfa99f
Dustin Glaser <dustin.glaser@posteo.de>
2023-11-24 15:17:52 +0100

    add documentation

--- src/main/java/h03/RobotSynchronizer.java
+++ src/main/java/h03/RobotSynchronizer.java
@@ -1,38 +1,74 @@
 package h03;
 
+import static fopbot.Direction.DOWN;
+import static fopbot.Direction.LEFT;
+import static fopbot.Direction.RIGHT;
+import static fopbot.Direction.UP;
+
 import fopbot.Direction;
-import static fopbot.Direction.*;
 import fopbot.Robot;
 import fopbot.World;
 
+/**
+ * A {@link RobotSynchronizer} is used to synchronize the position and direction of each robot of a set of robots
+ * with a given position and direction.
+ */
 public class RobotSynchronizer {
 
     private final Robot[] robots;
     private int x = -1, y = -1;
     private Direction direction = null;
 
+    /**
+     * Constructs a {@link RobotSynchronizer} for the given set of robots.
+     *
+     * @param robots the set of robots
+     */
     public RobotSynchronizer(Robot[] robots) {
         this.robots = robots;
     }
 
+    /**
+     * Sets the position on the x-axis to sync the robots with.
+     * If an invalid position is passed, the position will leave unchanged.
+     *
+     * @param x the position on the x-axis
+     */
     public void setX(int x) {
         if (x >= 0 && x < World.getWidth()) {
             this.x = x;
         }
     }
 
+    /**
+     * Sets the position on the y-axis to sync the robots with.
+     * If an invalid position is passed, the position will leave unchanged.
+     *
+     * @param y the position on the y-axis
+     */
     public void setY(int y) {
         if (y >= 0 && y < World.getHeight()) {
             this.y = y;
         }
     }
 
+    /**
+     * Sets the direction to sync the robots with.
+     * If <code>null</code> is passed, the direction will leave unchanged.
+     *
+     * @param direction the direction
+     */
     public void setDirection(Direction direction) {
         if (direction != null) {
             this.direction = direction;
         }
     }
 
+    /**
+     * Syncs each robot of the set of robots with the given position and direction.
+     * If the position on the x- or y-axis or the direction is not set,
+     * the current position/direction of the corresponding robot will be used instead.
+     */
     public void sync() {
         for (Robot r : robots) {
             int goalX = this.x != -1 ? this.x : r.getX();
@@ -47,7 +83,9 @@ public class RobotSynchronizer {
                 ) {
                     r.move();
                 }
-                if (goalDir == r.getDirection() && r.getX() == goalX && r.getY() == goalY) break;
+                if (goalDir == r.getDirection() && r.getX() == goalX && r.getY() == goalY) {
+                    break;
+                }
                 r.turnLeft();
             }
         }
